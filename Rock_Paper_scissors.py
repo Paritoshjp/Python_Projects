32f0f466963743745e5b11b7b1a3a4e7ecfc3570
Paritosh Shailesh Jain <jainparitosh15298@gmail.com>
2022-04-05 10:50:59 +0530

    Add files via upload

---
+++ Rock_Paper_scissors.py
@@ -0,0 +1,53 @@
+import random
+
+draw = 0
+user_wins = 0
+computer_wins = 0
+options = [ 'rock', 'paper', 'scissors']
+
+while True:
+    user_input = input("Please type Rock/Paper/Scissors or Q to Quit: ").lower()
+
+    if user_input == 'q':
+        break
+
+    if user_input not in options :
+        print("Enter valid response.")
+        continue
+
+    print("User picked: ", user_input)
+    random_number = random.randint(0,2)
+    #rock =0 , paper = 1 , scissors = 2
+    computer_pick = options[random_number]
+    print("Computer picked: ", computer_pick)
+
+    if user_input == 'rock' and computer_pick == 'scissors':
+        print("you won! ")
+        user_wins += 1
+        continue
+
+    elif user_input == 'paper' and computer_pick == 'rock':
+        print("you won! ")
+        user_wins += 1
+
+    elif user_input == 'scissors' and computer_pick == 'paper':
+        print("you won! ")
+        user_wins += 1
+
+    elif user_input == computer_pick:
+        print("Draw")
+        draw += 1
+
+    else:
+        print("you lost!")
+        computer_wins += 1
+
+print("You won ", user_wins , " times")
+print("Computer won ", computer_wins , " times")
+print("You drew with computer ", draw , " times")
+
+print("END")
+
+
+
+
